d3c0a17fabd9cf53d091d99eeb8535f20986cf4e
Ken Chung <ken.chung@thebaselab.com>
2022-04-11 01:34:19 +0800

    Update checksum

--- Package.swift
+++ Package.swift
@@ -3,20 +3,21 @@
 import PackageDescription
 
 let package = Package(
-	name: "Clibgit2",
-	platforms: [.iOS(.v13)],
-	products: [
-		.library(
-			name: "Clibgit2",
-			targets: [ "Clibgit2" ]
-		),
-	],
-	dependencies: [],
-	targets: [
-		.binaryTarget(
-			name: "Clibgit2",
-			url: "https://github.com/thebaselab/LibGit2-On-iOS/releases/download/v1.3.0/Clibgit2.xcframework.zip",
-			checksum: "411ed3c8ed48755e64b7f5611648cc3490f060ca39aff2c8a847b11d4a22d0fa"
-		),
-	]
+  name: "Clibgit2",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "Clibgit2",
+      targets: ["Clibgit2"]
+    )
+  ],
+  dependencies: [],
+  targets: [
+    .binaryTarget(
+      name: "Clibgit2",
+      url:
+        "https://github.com/thebaselab/LibGit2-On-iOS/releases/download/v1.3.0/Clibgit2.xcframework.zip",
+      checksum: "85b8440fd01de70a21fcd53fa6f7bbf7e4bf2d813730c87a81d5a461429e11ce"
+    )
+  ]
 )
