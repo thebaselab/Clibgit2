0abab9d7c7e050debf0ef7682a3e48199a0c3767
Ken Chung <ken.chung@thebaselab.com>
2022-09-17 13:19:45 +0800

    Update checksum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
       name: "Clibgit2",
       url:
         "https://github.com/thebaselab/LibGit2-On-iOS/releases/download/v1.3.1/Clibgit2.xcframework.zip",
-      checksum: "99db79d344d6d69bab7170345fe61ddaeca0a5e9ec4e316bfd558a1178b2af3e"
+      checksum: "a405cbf0b992e1469e2096a9c255aa9244e5063841ccdfa1f365c57a856f7182"
     )
   ]
 )
